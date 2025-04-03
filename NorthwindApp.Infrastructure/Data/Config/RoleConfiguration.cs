e8240c71ab322c9df7603ea762636fd2b1500bb5
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-04-03 23:05:03 +0200

    Add dbcontext and entity type config for user and role

---
+++ NorthwindApp.Infrastructure/Data/Config/RoleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NorthwindApp.Domain;
+
+namespace NorthwindApp.Infrastructure;
+
+public class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.Property(r => r.Abrv)
+            .HasMaxLength(DataSchemaConstants.DEFAULT_ABBREVIATION_LENGTH)
+            .IsRequired();
+        builder.Property(r => r.Name)
+            .HasMaxLength(DataSchemaConstants.DEFAULT_NAME_LENGTH)
+            .IsRequired();
+
+        builder.HasMany(r => r.Users)
+            .WithOne(u => u.Role)
+            .HasPrincipalKey(r => r.Id);
+    }
+}
---
+++ NorthwindApp.Infrastructure/Data/NorthwindAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindApp.Domain;
+using System.Reflection;
+
+namespace NorthwindApp.Infrastructure;
+
+public class NorthwindAppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
