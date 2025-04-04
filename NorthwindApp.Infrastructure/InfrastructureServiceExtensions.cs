87e357651e6b7fa77e75173daf431925bce64a80
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-04-04 23:41:49 +0200

    Add user controller

---
+++ NorthwindApp.Infrastructure/InfrastructureServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NorthwindApp.Application;
+
+namespace NorthwindApp.Infrastructure;
+
+public static class InfrastructureServiceExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfigurationManager configurationManager)
+    {
+        services.AddDbContext<NorthwindAppDbContext>(opts => 
+            opts.UseNpgsql(configurationManager.GetConnectionString("NorthwindApp")));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}
---
+++ NorthwindApp.WebApi/Configurations/ServiceConfigs.cs
@@ -0,0 +1,14 @@
+﻿using NorthwindApp.Infrastructure;
+
+namespace NorthwindApp.WebApi
+{
+    public static class ServiceConfigs
+    {
+        public static IServiceCollection AddServiceConfigs(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            services.AddInfrastructureServices(builder.Configuration);
+
+            return services;
+        }
+    }
+}
