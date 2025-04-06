85628ae2ffc554bf9c8a0743645c1b3c1112a79f
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-04-06 23:47:35 +0200

    Add UserCreateRestModel

---
+++ NorthwindApp.WebApi/RestModels/User/UserCreateRestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace NorthwindApp.WebApi;
+
+public class UserCreateRestModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public int RoleId { get; set; }
+    public string Password { get; set; }
+}
