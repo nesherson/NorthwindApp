7754e0db0e35d7fe6e5643cf2da4122f76b1e0fc
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-04-14 22:52:48 +0200

    Add router and paths

---
+++ northwindapp.client/src/app/router.tsx
@@ -0,0 +1,6 @@
+import { QueryClient, useQueryClient } from '@tanstack/react-query';
+import { useMemo } from 'react';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+
+import { paths } from '../config/paths';
