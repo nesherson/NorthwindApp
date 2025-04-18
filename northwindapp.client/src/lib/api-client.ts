0373c4156c2db7172cccb361cda3f18deada3504
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-04-18 23:43:41 +0200

    Add api functionality

--- northwindapp.client/src/lib/api-client.ts
+++ northwindapp.client/src/lib/api-client.ts
@@ -1,14 +1,14 @@
 import Axios, { InternalAxiosRequestConfig } from "axios";
 
-import { env } from "../config/env";
-import { paths } from "../config/paths";
+import { env } from "@/config/env";
+import { paths } from "@/config/paths";
 
 function authRequestInterceptor(config: InternalAxiosRequestConfig) {
 	if (config.headers) {
 		config.headers.Accept = "application/json";
 	}
 
-	config.withCredentials = true;
+	// config.withCredentials = true;
 	return config;
 }
 
@@ -22,7 +22,7 @@ api.interceptors.response.use(
       return response.data;
     },
     (error) => {
-      const message = error.response?.data?.message || error.message;
+      // const message = error.response?.data?.message || error.message;
     //   useNotifications.getState().addNotification({
     //     type: 'error',
     //     title: 'Error',
--- northwindapp.client/src/types/api.ts
+++ northwindapp.client/src/types/api.ts
@@ -3,49 +3,34 @@
 // with the backend instead of manually writing them out
 
 export type BaseEntity = {
-    id: string;
-    dateCreated: number;
-  };
-  
-  export type Entity<T> = {
-    [K in keyof T]: T[K];
-  } & BaseEntity;
-  
-  export type Meta = {
-    page: number;
-    total: number;
-    totalPages: number;
-  };
-  
-  export type User = Entity<{
-    firstName: string;
-    lastName: string;
-    email: string;
-    role: 'ADMIN' | 'USER';
-    teamId: string;
-    bio: string;
-  }>;
-  
-  export type AuthResponse = {
-    jwt: string;
-    user: User;
-  };
-  
-  export type Team = Entity<{
-    name: string;
-    description: string;
-  }>;
-  
-  export type Discussion = Entity<{
-    title: string;
-    body: string;
-    teamId: string;
-    author: User;
-  }>;
-  
-  export type Comment = Entity<{
-    body: string;
-    discussionId: string;
-    author: User;
-  }>;
-  
+	id: string;
+	dateCreated: Date;
+};
+
+export type Entity<T> = {
+	[K in keyof T]: T[K];
+} & BaseEntity;
+
+export type Meta = {
+	page: number;
+	total: number;
+	totalPages: number;
+};
+
+export type User = Entity<{
+	firstName: string;
+	lastName: string;
+	email: string;
+	dateOfBirth: Date,
+	role: Role;
+}>;
+
+export type AuthResponse = {
+	jwt: string;
+	user: User;
+};
+
+export type Role = {
+	abrv: string,
+	name: string
+}
