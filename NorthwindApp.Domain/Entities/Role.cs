ea862f27475040689b63d7f0e1eb2e49d2ef3c87
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-11-04 22:22:06 +0100

    Remove redundant files

--- NorthwindApp.Domain/Entities/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NorthwindApp.Domain;
-
-public class Role : BaseEntity
-{
-    public string Abrv { get; set; }
-    public string Name { get; set; }
-    public ICollection<User> Users { get; set; }
-}
